4606f11c38adbf1e1b385f810cd4cd3f944199f2
Derya Bel <deryaabel@hotmail.com>
2021-12-18 22:28:17 +0300

    Initial Commit

---
+++ SuperHeroSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SuperHeroSwiftUI
+//
+//  Created by Derya Bel on 18.12.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SuperHeroSwiftUI/SuperHeroSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperHeroSwiftUIApp.swift
+//  SuperHeroSwiftUI
+//
+//  Created by Derya Bel on 18.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct SuperHeroSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
